f717792c07344d955d016d17764f1ac2e7b9bdac
Vasily Myazin <vm@konspiredesign.com>
2012-03-21 21:48:03 -0700

    Updated jQ Mobile UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+  
+  def current_layout
+    layout = controller.send(:_layout)
+    if layout.instance_of? String
+      layout
+    else
+      File.basename(layout.identifier).split('.').first
+    end
+  end
+  
 end
